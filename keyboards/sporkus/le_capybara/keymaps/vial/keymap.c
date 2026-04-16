ff60a52e8680e54a90f92f0322664c21907b50b1
Sergey Fedorov <shade30@gmail.com>
2026-04-16 14:46:22 +0400

    Updated vial layout

--- keyboards/sporkus/le_capybara/keymaps/vial/keymap.c
+++ keyboards/sporkus/le_capybara/keymaps/vial/keymap.c
@@ -24,14 +24,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F1,            KC_F2,            KC_F3,            KC_F4,            KC_F5,            _______,         KC_F6,            KC_F7,            KC_F8,            KC_F9,            KC_F10,
         KC_EXLM,          KC_AT,            KC_HASH,          KC_DLR,           KC_PERC,                           KC_CIRC,          KC_AMPR,          KC_ASTR,          KC_LPRN,          KC_RPRN,
         KC_1,             KC_2,             KC_3,             KC_4,             KC_5,                              KC_6,             KC_7,             KC_8,             KC_9,             KC_0,
-                                             _______,          _______,          _______,                            _______,          _______,          _______
+                                            LT(_ADJ, KC_TAB), _______,          _______,                           _______,          _______,          LT(_ADJ, KC_DEL)
     ),
 
     [_RSE] = LAYOUT_3thumb(
         KC_PAST,          KC_P7,            KC_P8,            KC_P9,            KC_PPLS,          _______,         KC_CAPS,          KC_HOME,          KC_PSCR,          KC_PGUP,          S(KC_BSLS),
         LT(_WIN, KC_PSLS),LGUI_T(KC_P4),    LALT_T(KC_P5),    LCTL_T(KC_P6),    KC_PMNS,                           KC_LEFT,          RCTL_T(KC_DOWN),  RALT_T(KC_UP),    RGUI_T(KC_RGHT),  LT(_WIN, KC_INS),
         KC_P0,            KC_P1,            KC_P2,            KC_P3,            KC_PCMM,                           KC_SCRL,          KC_END,           KC_PAUS,          KC_PGDN,          KC_BSLS,
-                                            _______,          _______,          _______,                           _______,          _______,          _______
+                                            _______,          _______,          LT(_ADJ, KC_ENT),                  LT(_ADJ, KC_ESC), _______,          _______
     ),
 
     [_ADJ] = LAYOUT_3thumb(
